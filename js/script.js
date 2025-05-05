5a833438c27141abbfb46244ad64fe170ff7e31a
carlotalvs <carlotalova17@gmail.com>
2025-05-05 12:31:26 +0200

    qqq

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
 // LOGO MADTRENDS 
   $(document).ready(function() {
     const $logo = $('#logo');
-    const originalSrc = 'img/home/madtrendslogoblanco.png'; 
-    const hoverSrc = 'img/home/madtrendslogonegro.png'; 
-    const buttonHoverSrc = 'img/home/madtrendslogorosa.png'; 
+    const originalSrc = 'img/home/CUCAlogoblanco.png'; 
+    const hoverSrc = 'img/home/CUCAlogonegro.png'; 
+    const buttonHoverSrc = 'img/home/CUCAlogoverde.png'; 
     const $menu = $('.menu');
     const $navbarToggler = $('.navbar-toggler');
     const $submenu = $('#navbarNav');
